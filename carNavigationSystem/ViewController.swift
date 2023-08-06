eee20dfd1af0b17e287307bf7c6624e591a20238
Keita0920 <cy20255@shibaura-it.ac.jp>
2023-08-07 07:34:41 +0900

    最初の４つのボタンに文字を表示&画面遷移

--- carNavigationSystem/ViewController.swift
+++ carNavigationSystem/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var akasataButton: UIButton!
+    @IBOutlet var nahamayaButton: UIButton!
+    @IBOutlet var rawaButton: UIButton!
+    //@IBOutlet var label:UILabel!
+    var number:Int=0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        akasataButton.setTitle("あか\nさた", for: .normal)
+        nahamayaButton.setTitle("なは\nまや", for: .normal)
+        rawaButton.setTitle("らわ", for: .normal)
+        //label.text="s"
+
+    }
+    
+    @IBAction func plus(){
+        number=number + 1
+        //label.textColor=UIColor.black
     }
 
 
 }
-
